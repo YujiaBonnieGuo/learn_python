4681940bd32d2824c53dfe21c4a06f0abf9bb7f8
BonnieGuoYujia <31779590+BonnieGuoYujia@users.noreply.github.com>
2018-09-16 21:23:31 -0400

    age and gender

---
+++ ex10ageAndGender.py
@@ -0,0 +1,11 @@
+age=input("please input your age:")
+if int(age) >= 18:
+    print('adult')
+else:
+    print('teenager')
+
+
+
+age=input("please input your age: ")
+gender=input("please input your gender: ")
+print("your age is %s,your gender is %s" % (age,gender))
