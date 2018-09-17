eea418aee76182b5d63f3d9bc2f4553f61b0270d
BonnieGuoYujia <31779590+BonnieGuoYujia@users.noreply.github.com>
2018-09-17 10:48:36 -0400

    changing name

---
+++ ex03printmultilines.py
@@ -0,0 +1,13 @@
+#print absolute value of an integer
+a=-100
+if a>=0:
+    print(a)
+else:
+	print(-a)
+
+print('''line1
+line2
+line3''')
+
+t_007 = 'T007'
+print(t_007)
---
+++ ex07output.py
@@ -0,0 +1,2 @@
+print('Hello, %s' % 'world')
+print('Hi,%s,you have $%d'%('michael',100))
