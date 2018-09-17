27c06dee2b59c5175ebf474a63ae7091812b1de7
BonnieGuoYujia <31779590+BonnieGuoYujia@users.noreply.github.com>
2018-09-16 21:20:40 -0400

    changing lines

---
+++ ex9.py
@@ -0,0 +1,8 @@
+#here's some new strange stuff,remember type it exactly
+
+days="Mon Thu Wed Thu Fri Sat Sun"
+months="Jan\nFeb\nMar\nApr\nMay\nJune\nJuly\nAug"
+
+print("here are the days: ",days)
+print("here are the months:\n",months)
+print("there are something going on here\nwhith the three double-quotes\nwe'll be able to type as much as we like")
