9a3c342a1414f38547c7587ec27fc84f502b90a5
YujiaBonnieGuo <bonnieguo1994@gmail.com>
2018-09-27 14:45:18 -0400

    correct the output

--- ex35定制类.py
+++ ex35定制类.py
@@ -19,4 +19,8 @@ class Student(object):
     def __str__(self):
         return 'Student object (name: %s)' % self.name
 print(Student('Michael'))
-        
+# __str__()返回用户看到的字符串，而__repr__()返回程序开发者看到的字符串，
+# 也就是说，__repr__()是为调试服务的
+
+# 再定义一个__repr__()。但是通常__str__()和__repr__()代码都是一样的，
+# 所以，有个偷懒的写法：
