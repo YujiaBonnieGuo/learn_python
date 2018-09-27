1bf423169c2ab5671dc0f6b0ad3a4a96009ceab8
YujiaBonnieGuo <bonnieguo1994@gmail.com>
2018-09-26 23:48:36 -0400

    str add a class

--- ex35定制类.py
+++ ex35定制类.py
@@ -10,3 +10,6 @@ class Student(object):
     def __init__(self, name):
         self.name = name
 print(Student('Michael'))
+# 打印出一堆<__main__.Student object at 0x109afb190>，不好看。
+
+# 怎么才能打印得好看呢？只需要定义好__str__()方法，返回一个好看的字符串就可以了：
