e1fe692b9c5f002f5d08fca732865eeac8295fe4
YujiaBonnieGuo <bonnieguo1994@gmail.com>
2018-10-01 14:00:16 -0400

    err_raise.py

--- ex40记录错误.py
+++ ex40记录错误.py
@@ -20,3 +20,21 @@ def main():
 
 main()
 print('END')
+#样是出错，但程序打印完错误信息后会继续执行，并正常退出：
+#通过配置，logging还可以把错误记录到日志文件里，方便事后排查。
+
+#抛出错误
+# 因为错误是class，捕获一个错误就是捕获到该class的一个实例。因此，错误并不是凭空产生的，而是有意创建并抛出的。Python的内置函数会抛出很多类型的错误，我们自己编写的函数也可以抛出错误。
+
+# 如果要抛出错误，首先根据需要，可以定义一个错误的class，选择好继承关系，然后，用raise语句抛出一个错误的实例：
+# err_raise.py
+class FooError(ValueError):
+    pass
+
+def foo(s):
+    n = int(s)
+    if n==0:
+        raise FooError('invalid value: %s' % s)
+    return 10 / n
+
+foo('0')
