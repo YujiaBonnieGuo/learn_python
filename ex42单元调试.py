ce0b7dfd9304c61205266bddc1a83c06a0e78b65
YujiaBonnieGuo <bonnieguo1994@gmail.com>
2018-10-12 16:51:26 -0400

    mydict

--- ex42单元调试.py
+++ ex42单元调试.py
@@ -7,16 +7,4 @@
 # 1
 # mydict.py代码如下：
 
-class Dict(dict):
 
-    def __init__(self, **kw):
-        super().__init__(**kw)
-
-    def __getattr__(self, key):
-        try:
-            return self[key]
-        except KeyError:
-            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
-
-    def __setattr__(self, key, value):
-        self[key] = value
