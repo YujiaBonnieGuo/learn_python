e075c596f5fc81ee8c1cedbbf5a83dee45268658
YujiaBonnieGuo <bonnieguo1994@gmail.com>
2018-10-19 09:47:00 -0400

    open function

---
+++ open_function.py
@@ -0,0 +1,5 @@
+To open a file in Python, we use the open() function. This function accepts two different arguments (inputs) in the parentheses, always in the following order:
+
+the name of the file (as a string)
+the mode of working with the file (as a string)
+We'll learn about the various modes in a later mission. For now, we'll just use "r", the mode for reading in files.
