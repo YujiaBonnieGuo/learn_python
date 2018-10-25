671f4f69e68e66d3c067dd87045172e3bf061117
YujiaBonnieGuo <bonnieguo1994@gmail.com>
2018-10-25 16:41:01 -0400

    changing

--- ex34多重继承.py
+++ ex34多重继承.py
@@ -52,7 +52,7 @@ class pet(Animal):
 class Dog(Mammal, RunnableMixIn, CarnivorousMixIn):
     pass
 CarnivorousMixIn
-
+# MixIn的目的就是给一个类增加多个功能，这样，在设计类的时候，我们优先考虑通过多重继承来组合多个MixIn的功能，而不是设计多层次的复杂的继承关系。
 
 # Python自带的很多库也使用了MixIn。举个例子，Python自带了TCPServer和UDPServer这两类网络服务，而要同时服务多个用户就必须使用多进程或多线程模型，这两种模型由ForkingMixIn和ThreadingMixIn提供。通过组合，我们就可以创造出合适的服务来。
 
