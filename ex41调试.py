fcd40f6b041b4369b0087af271a785e7887bfc56
YujiaBonnieGuo <bonnieguo1994@gmail.com>
2018-10-27 10:58:35 -0400

    changing

--- ex41调试.py
+++ ex41调试.py
@@ -31,7 +31,8 @@ import logging
 logging.basicConfig(level=logging.INFO)
 s='0'
 n=int(s)
-
+logging.info('n=%d'%n)
+print(10/n)
 # 这就是logging的好处，它允许你指定记录信息的级别，有debug，info，warning，error等几个级别，当我们指定level=INFO时，logging.debug就不起作用了。同理，指定level=WARNING后，debug和info就不起作用了。这样一来，你可以放心地输出不同级别的信息，也不用删除，最后统一控制输出哪个级别的信息。
 
 # logging的另一个好处是通过简单的配置，一条语句可以同时输出到不同的地方，比如console和文件。
@@ -40,9 +41,9 @@ n=int(s)
 # 第4种方式是启动Python的调试器pdb，让程序以单步方式运行，可以随时查看运行状态。我们先准备好程序：
 
 # # err.py
-# s = '0'
-# n = int(s)
-# print(10 / n)
+s = '0'
+n = int(s)
+print(10 / n)
 # 然后启动：
 
 # $ python -m pdb err.py
