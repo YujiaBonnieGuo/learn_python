e42a2bb91ae6a539e7485a2ff5905912509c91a2
YujiaBonnieGuo <bonnieguo1994@gmail.com>
2019-02-25 10:51:01 -0500

    note

---
+++ note for deeplearning.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Data preproessing
+# Importng the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importting the dataset
+datasets=pd.read_csv('Data.csv')
+X = datasets.iloc[:,:-1].values#all columns except the last one
+y = datasets.iloc[:,3].values#iport the last column
+
+# Taking care of missing data
+from sklearn.preprocessing import Imputer#contains methods of preprocessing
+imputer = Imputer(missing_values='NaN', strategy = 'mean', axis = 0, )
+imputer = imputer.fit(X[:,1:3])
+X[:,1:3] = imputer.transform(X[:,1:3] )
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:,0] = labelencoder_X.fit_transform(X[:,0])
+#为了避免将文字替换成数字时候，数字产生排序的意义，进行以下操作：
+#制造三列，每一列代表一个国家，只有数字一存在 dummy bariables
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+#最后一列可以用0/1表示
+labelencoder_y = LabelEncoder()
+y= labelencoder_y.fit_transform(y)
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-cities = ["Albuquerque", "Anaheim", "Anchorage", "Arlington", "Atlanta"]
-crime_rates = [749, 371, 828, 503, 1379]
-cities_slice=cities[1:4]
-ending_index=len(crime_rates)
-cr_slice=crime_rates[ending_index-2:ending_index]
-print(cities_slice,'\n',cr_slice)
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importting the dataset
+datasets=pd.read_csv('Data.csv')
+X = datasets.iloc[:,:-1].values#all columns except teh last one
