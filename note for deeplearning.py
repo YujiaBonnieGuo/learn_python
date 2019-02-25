f0c103a909c81eb05ea2c832b587beb7ec3d8647
YujiaBonnieGuo <bonnieguo1994@gmail.com>
2019-02-25 10:51:58 -0500

    note

--- note for deeplearning.py
+++ note for deeplearning.py
@@ -17,13 +17,13 @@ imputer = imputer.fit(X[:,1:3])
 X[:,1:3] = imputer.transform(X[:,1:3] )
 
 # Encoding categorical data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_X = LabelEncoder()
-X[:,0] = labelencoder_X.fit_transform(X[:,0])
-#为了避免将文字替换成数字时候，数字产生排序的意义，进行以下操作：
-#制造三列，每一列代表一个国家，只有数字一存在 dummy bariables
-onehotencoder = OneHotEncoder(categorical_features = [0])
-X = onehotencoder.fit_transform(X).toarray()
-#最后一列可以用0/1表示
-labelencoder_y = LabelEncoder()
-y= labelencoder_y.fit_transform(y)
+# from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+# labelencoder_X = LabelEncoder()
+# X[:,0] = labelencoder_X.fit_transform(X[:,0])
+# #为了避免将文字替换成数字时候，数字产生排序的意义，进行以下操作：
+# #制造三列，每一列代表一个国家，只有数字一存在 dummy bariables
+# onehotencoder = OneHotEncoder(categorical_features = [0])
+# X = onehotencoder.fit_transform(X).toarray()
+# #最后一列可以用0/1表示
+# labelencoder_y = LabelEncoder()
+# y= labelencoder_y.fit_transform(y)
