dfdfac404b407ac7e79a08ef305ab89cf0c69b4a
YujiaBonnieGuo <bonnieguo1994@gmail.com>
2019-02-27 15:02:46 -0500

    note

--- note for deeplearning.py
+++ note for deeplearning.py
@@ -21,16 +21,16 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
 X[:,0] = labelencoder_X.fit_transform(X[:,0])
 #为了避免将文字替换成数字时候，数字产生排序的意义，进行以下操作：
-#制造三列，每一列代表一个国家，只有数字一存在 dummy bariables
-onehotencoder = OneHotEncoder(categorical_features = [0])
-X = onehotencoder.fit_transform(X).toarray()
-#最后一列可以用0/1表示
-labelencoder_y = LabelEncoder()
-y= labelencoder_y.fit_transform(y)
+# #制造三列，每一列代表一个国家，只有数字一存在 dummy bariables
+# onehotencoder = OneHotEncoder(categorical_features = [0])
+# X = onehotencoder.fit_transform(X).toarray()
+# #最后一列可以用0/1表示
+# labelencoder_y = LabelEncoder()
+# y= labelencoder_y.fit_transform(y)
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X , y,test_size = 0.2, random_state = 0)
+# # Splitting the dataset into the Training set and Test set
+# from sklearn.cross_validation import train_test_split
+# X_train, X_test, y_train, y_test = train_test_split(X , y,test_size = 0.2, random_state = 0)
 
 # Feature scaling
 from sklearn.preprocessing import StandardScaler
