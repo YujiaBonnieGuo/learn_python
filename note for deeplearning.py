018444f38784cf0c621a9e1f2778f363f9161d16
YujiaBonnieGuo <bonnieguo1994@gmail.com>
2019-03-05 10:52:52 -0500

    note

--- note for deeplearning.py
+++ note for deeplearning.py
@@ -19,7 +19,7 @@ X[:,1:3] = imputer.transform(X[:,1:3] )
 # Encoding categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
-# X[:,0] = labelencoder_X.fit_transform(X[:,0])
+X[:,0] = labelencoder_X.fit_transform(X[:,0])
 #为了避免将文字替换成数字时候，数字产生排序的意义，进行以下操作：
 #制造三列，每一列代表一个国家，只有数字一存在 dummy bariables
 onehotencoder = OneHotEncoder(categorical_features = [0])
